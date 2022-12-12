2e3ed4e8c9ddb846dafebe1f83adef200ca68ad9
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2022-12-12 16:38:30 -0300

    Controle

--- main/components/delay/delay.h
+++
@@ -1,6 +0,0 @@
-#ifndef DELAY_H
-#define DELAY_H
-
-extern void vDelayMs(int ms);
-
-#endif
--- main/src/motors.cpp
+++ main/src/motors.cpp
@@ -15,9 +15,9 @@
 
 #define ACCELEROMETER_DEGREES_TO_RADIAN M_PI/180
 
-#define Kd                              23.43
-#define Ki                              0.15
-#define Kp                              1.17
+#define Kd                              79.52
+#define Ki                              1
+#define Kp                              126
 #define X                               0.0425
 #define Ts                              0.05
 
@@ -31,6 +31,7 @@ static float OUT_CONTROL_PAST2 = 0;
 static float ERROR_CONTROL = 0;
 static float ERROR_CONTROL_PAST1 = 0;
 static float ERROR_CONTROL_PAST2 = 0;
+
 
 
 // float i = 31;
@@ -63,5 +64,26 @@ void vAdjustMotors(void) {
 
      float angle = fGetCurrentAngle();
      ESP_LOGI("Angle", "%.2f", angle);
+
+     if(angle > -14 && angle < 14) {
+         
+     
+          ERROR_CONTROL = REF_ANGLE - angle;
+          ERROR_CONTROL = ERROR_CONTROL*ACCELEROMETER_DEGREES_TO_RADIAN;
+
+          float OUT_CONTROL = ERROR_CONTROL*(2*Ts*Kp+Ki*pow(Ts,2)+4*Kd) + ERROR_CONTROL_PAST1*(2*Ki*pow(Ts,2)-8*Kd) + ERROR_CONTROL_PAST2*(Ki*pow(Ts,2)-2*Ts*Kp+4*Kd) - OUT_CONTROL_PAST2*(2*Ts);
+          float pwm = mcpwm_get_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B);
+          float out_pwm = abs(OUT_CONTROL*2.3134);
+
+          out_pwm = 30*pwm/100 + 30;
+
+          mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, out_pwm);
+
+          ERROR_CONTROL_PAST2 = ERROR_CONTROL_PAST1;
+          ERROR_CONTROL_PAST1 = ERROR_CONTROL;
+          OUT_CONTROL_PAST2 = OUT_CONTROL_PAST1;
+          OUT_CONTROL_PAST1 = OUT_CONTROL;
+
+     }
 
 }
